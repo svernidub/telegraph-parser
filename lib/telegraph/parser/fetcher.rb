45a9913a1d30dd97a6f9e3f11d2032317cc5262b
Serhii Vernydub <svernidub@gmail.com>
2018-08-15 08:51:45 +0300

    Add support for HTTPS queries.
    Telegraph changes schema from HTTP to HTTPS so fetcher breakes
    on article requests.

--- lib/telegraph/parser/fetcher.rb
+++ lib/telegraph/parser/fetcher.rb
@@ -6,18 +6,34 @@ module Telegraph
       HOST = 'telegra.ph'.freeze
 
       def fetch(article_id)
-        Net::HTTP.get(HOST, "/#{article_id}")
+        response = Net::HTTP.get_response(article_uri(article_id))
+        handle_response(response)
       rescue EOFError
         raise Telegraph::Parser::ArticleNotFound
       end
 
       def fetch_image(src, prefix)
-        src = src.start_with?('http') ? src : "http://#{HOST}#{src}"
+        src = src.start_with?('http') ? src : "https://#{HOST}#{src}"
         { image_id(src, prefix) => open(src).read }
       end
 
       def image_id(src, prefix)
         "#{prefix}/#{File.basename(URI.parse(src).path)}"
+      end
+
+      private
+
+      def article_uri(article_id)
+        URI("https://#{HOST}/#{article_id}")
+      end
+
+      def handle_response(response)
+        case response.code
+        when '404'
+          raise Telegraph::Parser::ArticleNotFound
+        when '200'
+          response.body
+        end
       end
     end
   end
